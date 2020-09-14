af79913177b92d72bf8e0930147e3600ea5fa2b6
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2020-09-14 13:23:51 -0500

    Updated packages

--- MoonbounceDependencies/Package.swift
+++ MoonbounceDependencies/Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "MoonbounceDependencies",
+    platforms: [.macOS(.v10_15)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -13,19 +14,20 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/weichsel/ZIPFoundation", from: "0.9.6"),
-         .package(url: "https://github.com/OperatorFoundation/Shapeshifter-Swift-Transports", from: "0.3.10"),
-         .package(url: "https://github.com/OperatorFoundation/Datable", from: "1.0.5"),
-         .package(url: "https://github.com/OperatorFoundation/SwiftQueue.git", from: "0.0.1")
+         .package(url: "https://github.com/weichsel/ZIPFoundation", from: "0.9.11"),
+         .package(url: "https://github.com/OperatorFoundation/Shapeshifter-Swift-Transports", from: "2.2.8"),
+         .package(url: "https://github.com/OperatorFoundation/Datable", from: "3.0.2"),
+         .package(url: "https://github.com/OperatorFoundation/SwiftQueue.git", from: "0.0.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "MoonbounceDependencies",
-            dependencies: ["ZIPFoundation", "Datable", "Replicant", "Flow", "SwiftQueue"]),
+            dependencies: ["ZIPFoundation", "Datable", .product(name: "Replicant", package: "Shapeshifter-Swift-Transports"), .product(name: "Flow", package: "Shapeshifter-Swift-Transports"), "SwiftQueue"]),
         .testTarget(
             name: "MoonbounceDependenciesTests",
             dependencies: ["MoonbounceDependencies"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
