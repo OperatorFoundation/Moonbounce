6faf3f2cd08b3372a3b7a02c06d003a2942b42bd
CryptoSax <joshua@operatorfoundation.org>
2022-04-08 16:31:08 -0500

    removed unused code

--- MoonbounceApplication/Globals.swift
+++ MoonbounceApplication/Globals.swift
@@ -12,57 +12,51 @@ import Net
 
 public var appLog = Logger(label: "org.OperatorFoundation.Moonbounce.MacOS")
 
-public let mbPink = NSColor(red:0.92, green:0.55, blue:0.73, alpha:1.0)
 public let mbDarkBlue = NSColor(red:0.00, green:0.06, blue:0.16, alpha:1.0)
-public let mbBlue = NSColor(red:0.16, green:0.20, blue:0.48, alpha:1.0)
 public let mbWhite = NSColor(red:0.97, green:0.97, blue:0.98, alpha:1.0)
 
 public let kConnectionStatusNotification = "ConnectionStatusChanged"
-public let kServerIPAvailableNotification = "PTServerIPAvailable"
+//public let kServerIPAvailableNotification = "PTServerIPAvailable"
 public let kNewServerAddedNotification = "NewServerHasBeenAdded"
-public let serverManagerReadyNotification = "ServerFinishedInit"
-public let userTokenKey = "UserDoToken"
-public let userDirectoryName = "User"
-public let importedDirectoryName = "Imported"
-public let defaultDirectoryName = "Default"
+//public let serverManagerReadyNotification = "ServerFinishedInit"
+//public let userTokenKey = "UserDoToken"
+//public let userDirectoryName = "User"
+//public let importedDirectoryName = "Imported"
+//public let defaultDirectoryName = "Default"
 public let defaultTunnelName = "Default"
-public let clientConfigFileName = "replicantclient.config"
-public let replicantConfigFileName = "replicant.config"
-public let moonbounceExtension = "moonbounce"
+//public let moonbounceExtension = "moonbounce"
 
 //let serverManager = ServerController(completionHandler: {NotificationCenter.default.post(Notification(name: Notification.Name(serverManagerReadyNotification)))})
 
-public let appSupportDirectory = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first
-
-public var moonbounceDirectory = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent("Moonbounce.macOS", isDirectory: true)
+//public var moonbounceDirectory = FileManager.default.homeDirectoryForCurrentUser.appendingPathComponent("Moonbounce.macOS", isDirectory: true)
 
 // Parent config files directory - One directory to rule them all
-public let configFilesDirectory = moonbounceDirectory.appendingPathComponent("ConfigFiles", isDirectory: true)
+//public let configFilesDirectory = moonbounceDirectory.appendingPathComponent("ConfigFiles", isDirectory: true)
 
 //Default Config file directory - This is the config we supply for the demo server we run
-public let defaultConfigDirectory = configFilesDirectory.appendingPathComponent(defaultDirectoryName
-    , isDirectory: true)
+//public let defaultConfigDirectory = configFilesDirectory.appendingPathComponent(defaultDirectoryName
+//   , isDirectory: true)
 
 // Imported config files directory - Configurations imported by the user via the file system
-public var importedConfigDirectory = configFilesDirectory.appendingPathComponent(importedDirectoryName, isDirectory: true)
+// public var importedConfigDirectory = configFilesDirectory.appendingPathComponent(importedDirectoryName, isDirectory: true)
 
 // User Config Directory - Created when the user launches a Digital Ocean server through our app
-public var userConfigDirectory = configFilesDirectory.appendingPathComponent(userDirectoryName + "/DO", isDirectory: true)
+// public var userConfigDirectory = configFilesDirectory.appendingPathComponent(userDirectoryName + "/DO", isDirectory: true)
 
 //var currentConfigDirectory = defaultConfigDirectory
 
-public var currentHost: String?
-public var userHost: String?
-{
-    didSet
-    {
-        if userHost != nil
-        {
-            appLog.debug("Changed Global var for server IP: \(userHost!)")
-            NotificationCenter.default.post(name: NSNotification.Name(rawValue: kServerIPAvailableNotification), object: userHost!)
-        }
-    }
-}
+//public var currentHost: String?
+//public var userHost: String?
+//{
+//    didSet
+//    {
+//        if userHost != nil
+//        {
+//            appLog.debug("Changed Global var for server IP: \(userHost!)")
+//            NotificationCenter.default.post(name: NSNotification.Name(rawValue: kServerIPAvailableNotification), object: userHost!)
+//        }
+//    }
+//}
 
 public var isConnected = ConnectState(state: .start, stage: .start)
 {
